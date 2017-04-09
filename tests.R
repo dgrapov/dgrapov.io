0d64a6e05d5057457c31edc83dc933b1c66be43b
Dmitry Grapov <dgrapov@gmail.com>
2017-04-08 22:36:44 -0500

    init

---
+++ tests.R
@@ -0,0 +1,20 @@
+#tests
+#https://proquestionasker.github.io/blog/Making_Site/
+# install.packages('digest')
+# devtools::install_github('rstudio/blogdown')
+library(blogdown)
+
+#get hugo
+blogdown::install_hugo()
+
+#new site
+setwd('site')
+blogdown::new_site()
+
+#blogdown::install_theme('jrutheiser/hugo-lithium-theme') # rename to remove -master
+# blogdown::install_theme('sethmacleod/dimension')
+# blogdown::install_theme('saey55/hugo-elate-theme')
+blogdown::serve_site()
+
+#for gh pages need  docs or gh-pages folder  for public move public up
+
